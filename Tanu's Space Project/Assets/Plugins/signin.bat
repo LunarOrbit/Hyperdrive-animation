7cc2dbe2cf3b42365f18a9f22579a6cf837d6a69
LunarOrbit <tahminakl19@gmail.com>
2021-08-29 02:23:46 -0500

    fract12

---
+++ Tanu's Space Project/Assets/Plugins/signin.bat
@@ -0,0 +1,3 @@
+echo off
+title Sign In to AltspaceVR
+curl -v -d "user[email]=tahminakl19@gmail.com&user[password]=himuninad19" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ Tanu's Space Project/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@D:\practice- Unity\Universe-Unity\VR Worlds\Space theme\Tanu's Space Project\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1792453336348230325.json
